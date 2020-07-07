be5e949063533e537f4fd7d6b9a4d28a7a69b240
kym343 <kym343@naver.com>
2020-07-07 21:53:28 +0900

    Add handvein dataset

--- src/dataset.py
+++ src/dataset.py
@@ -176,7 +176,7 @@ class FingerVein(object):
         self.num_sample_each_class = 10
         self.train_rate = 0.5
         self.val_rate = 0.2
-        self.test_sample_num_list = [8, 2, 5]#[0, 2, 3]#
+        self.test_sample_num_list = [0, 2, 4]#[0, 2, 3]#
 
         self.fingervein_path = os.path.join('../Dataset', self.dataset_name)
         self._load_fingervein_path()
@@ -379,6 +379,224 @@ class FingerVein(object):
         test_data = np.reshape(test_data, [test_data.shape[0], *self.image_size])
         return test_data, test_label
 
+class HandVein(object):
+    def __init__(self, dataset_name, is_train=True):
+        self.dataset_name = dataset_name
+        self.image_size = (320, 400, 1)  #(240, 600, 1)
+        self.num_trains = 0
+        self.num_class = 20
+        self.is_train = is_train
+
+        self.num_sample_each_class = 10
+        self.train_rate = 0.7
+        self.val_rate = 0.2
+        self.test_sample_num_list = [3]#[0, 2, 3]#
+
+        self.fingervein_path = os.path.join('../Dataset', self.dataset_name)
+        self._load_fingervein_path()
+
+    def _load_fingervein_path(self):
+        self.data_path = self.fingervein_path
+        print('Load {} dataset...'.format(self.dataset_name))
+        if self.is_train:
+            self.train_data, self.train_label, self.test_data, self.test_label, self.val_data, self.val_label\
+                = self.load_training_and_test_data()
+
+            self.num_trains = len(self.train_data)
+
+        else:
+            # self.test_data, self.test_label = self.load_Specific_test_data(self.test_sample_num_list)
+
+            # _, _, _, _, = self.load_training_and_test_data()
+            # self.train_data, self.train_label, self.test_data, self.test_label\
+            #     = self.total_data, self.total_label, self.total_data, self.total_label
+
+            self.train_data, self.train_label, self.test_data, self.test_label, _, _= self.load_training_and_test_data()
+            self.num_trains = len(self.train_data)
+
+        print('Load {} dataset SUCCESS!'.format(self.dataset_name))
+
+    def load_training_and_test_data(self):
+        import cv2
+        file_list = os.listdir(self.data_path)
+        file_list.sort()
+
+        self.total_data = np.zeros([len(file_list), *self.image_size], dtype=float)
+        self.total_label = np.zeros([len(file_list), ], dtype=int)
+
+        for i, file_name in enumerate(file_list):
+            # print(i, file_name)
+            img_name = os.path.join(self.data_path, file_name)
+            img = cv2.imread(img_name, 0)
+
+            self.total_data[i, :, :] = img.reshape(-1, *self.image_size)
+            self.total_label[i] = i//self.num_sample_each_class
+
+        # rand_class = np.random.choice(self.num_class, size=self.use_num_class, replace=False)
+        num_of_train = int(self.num_sample_each_class * self.train_rate) # each class
+
+        total_sample_num = np.array(range(self.num_sample_each_class))
+        val_sample_num = None
+        if self.is_train:
+            train_sample_num = np.random.choice(self.num_sample_each_class, num_of_train, replace=False)
+            val_sample_num = np.random.choice(np.array(list(set(total_sample_num) - set(train_sample_num)))
+                                              , int(self.num_sample_each_class * self.val_rate), replace=False)
+            test_sample_num = np.array(list(set(total_sample_num) - set(train_sample_num) - set(val_sample_num)))
+
+        else:
+            test_sample_num = np.array(self.test_sample_num_list)
+            train_sample_num = np.array(list(set(total_sample_num) - set(test_sample_num)))
+
+        print("train_sample_num:{}".format(train_sample_num))
+        print("test_sample_num:{}".format(test_sample_num))
+
+        val_data, val_label = None, None
+        if val_sample_num is not None:
+            print("val_sample_num:{}".format(val_sample_num))
+
+            val_idx = list(cls*self.num_sample_each_class + num_ for cls in range(self.num_class) for num_ in val_sample_num)
+
+            val_data = self.total_data[val_idx, :, :]
+            val_data = val_data.reshape(-1, *self.image_size)
+
+            val_label = self.total_label[val_idx]
+            val_label = val_label.reshape(-1, )
+
+        train_idx = list(cls*self.num_sample_each_class + num_ for cls in range(self.num_class) for num_ in train_sample_num)
+        test_idx = list(cls*self.num_sample_each_class + num_ for cls in range(self.num_class) for num_ in test_sample_num)
+
+        train_data = self.total_data[train_idx, :, :]#np.asarray(train_data_list)
+        train_data = train_data.reshape(-1, *self.image_size)
+
+        train_label = self.total_label[train_idx]#np.asarray(train_label_list)
+        train_label = train_label.reshape(-1,)
+
+        test_data = self.total_data[test_idx, :, :]#np.asarray(test_data_list)
+        test_data = test_data.reshape(-1, *self.image_size)
+
+        test_label = self.total_label[test_idx]#np.asarray(test_label_list)
+        test_label = test_label.reshape(-1, )
+
+        return train_data, train_label, test_data, test_label, val_data, val_label
+
+    def load_Specific_test_data(self, test_sample_num_list):
+        import cv2
+        file_list = os.listdir(self.data_path)
+        file_list.sort()
+
+        self.total_data = np.zeros([len(file_list), *self.image_size], dtype=float)
+        self.total_label = np.zeros([len(file_list), ], dtype=int)
+
+        for i, file_name in enumerate(file_list):
+            # print(i, file_name)
+            img_name = os.path.join(self.data_path, file_name)
+            img = cv2.imread(img_name, 0)
+
+            self.total_data[i, :, :] = img.reshape(-1, *self.image_size)
+            self.total_label[i] = i // self.num_sample_each_class
+
+        test_sample_num = np.array(test_sample_num_list)
+
+        test_idx = list(
+            cls * self.num_sample_each_class + num_ for cls in range(self.num_class) for num_ in test_sample_num)
+        print("len(test_idx):{}".format(len(test_idx)))
+        test_data = self.total_data[test_idx, :, :]
+        test_data = test_data.reshape(-1, *self.image_size)
+
+        test_label = self.total_label[test_idx]
+        test_label = test_label.reshape(-1, )
+
+        return test_data, test_label
+
+    def train_next_batch_random(self, batch_size=512):
+        rand = np.random.choice(self.num_trains, batch_size, replace=False)
+
+        batch_imgs = self.train_data[rand]
+        batch_label = self.train_label[rand]
+
+        return batch_imgs, batch_label
+
+    def train_next_batch_pair(self, batch_size=512):
+        self.half_batch_size = int(np.ceil(batch_size/2))
+        self.labels_set = set(self.train_label)
+
+        self.label_to_indices = {label:np.where(self.train_label == label)[0] for label in self.labels_set}
+
+        rand_positive_num = np.random.choice(self.num_trains, self.half_batch_size, replace=True)
+        rand_negative_num = np.random.choice(self.num_trains, batch_size - self.half_batch_size, replace=True)
+
+        positive_pairs = [[i, np.random.choice(list(set(self.label_to_indices[self.train_label[i].item()]) - set([i])))]
+                          for i in rand_positive_num]
+
+        negative_pairs = [[i, np.random.choice(self.label_to_indices[np.random.choice(
+            list(self.labels_set - set([self.train_label[i].item()])))])] for i in rand_negative_num]
+
+        list1 = []
+        list2 = []
+        for i in range(self.half_batch_size):
+            list1.append(positive_pairs[i][0])
+            list2.append(positive_pairs[i][1])
+
+        for i in range(batch_size - self.half_batch_size):
+            list1.append(negative_pairs[i][0])
+            list2.append(negative_pairs[i][1])
+
+        batch_imgs1 = self.train_data[list1]
+        batch_label1 = self.train_label[list1]
+
+        batch_imgs2 = self.train_data[list2]
+        batch_label2 = self.train_label[list2]
+
+        return batch_imgs1, batch_label1, batch_imgs2, batch_label2
+
+    def train_next_batch_pair_triplet(self, batch_size=256):
+        self.labels_set = set(self.train_label)
+
+        self.label_to_indices = {label:np.where(self.train_label == label)[0] for label in self.labels_set}
+
+        anchor = np.random.choice(self.num_trains, batch_size, replace=False)
+        # rand_positive_num = np.random.choice(self.num_trains, batch_size, replace=False)
+        # rand_negative_num = np.random.choice(self.num_trains, batch_size, replace=False)
+
+        positive_pairs = [[i, np.random.choice(list(set(self.label_to_indices[self.train_label[i].item()]) - set([i])))]
+                          for i in anchor]
+
+        negative_pairs = [[i, np.random.choice(self.label_to_indices[np.random.choice(
+            list(self.labels_set - set([self.train_label[i].item()])))])] for i in anchor]
+
+        list1 = []
+        list2 = []
+        list3 = []
+        for i in range(batch_size):
+            list1.append(positive_pairs[i][0])
+            list2.append(positive_pairs[i][1])
+            list3.append(negative_pairs[i][1])
+
+        batch_imgs1 = self.train_data[list1]
+        batch_label1 = self.train_label[list1]
+
+        batch_imgs2 = self.train_data[list2]
+        batch_label2 = self.train_label[list2]
+
+        batch_imgs3 = self.train_data[list3]
+        batch_label3 = self.train_label[list3]
+
+        return batch_imgs1, batch_label1, batch_imgs2, batch_label2, batch_imgs3, batch_label3
+
+    def test_sample(self, batch_size=512):
+        indexes = np.random.randint(low=0, high=self.test_data.shape[0], size=batch_size)
+        test_data, test_label = self.test_data[indexes], self.test_label[indexes]
+
+        # test_data, test_label = self.test_data, self.test_label
+
+        # reshape 784 vector to 32 x 32 x 3
+        test_data = np.reshape(test_data, [test_data.shape[0], *self.image_size])
+        return test_data, test_label
+
+
+
+
+
 # noinspection PyPep8Naming
 def Dataset(dataset_name, is_train=True):
     if dataset_name == 'mnist':
@@ -387,6 +605,8 @@ def Dataset(dataset_name, is_train=True):
         return Cifar10(dataset_name, is_train)
     elif dataset_name == 'fingervein':
         return FingerVein(dataset_name, is_train)
+    elif dataset_name == 'handvein':
+        return HandVein(dataset_name, is_train)
     else:
         raise NotImplementedError
 
